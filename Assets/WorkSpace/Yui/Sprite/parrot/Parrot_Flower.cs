6b4f20c035647f231d31eed7695856a1ca6ab97b
Yuishun <shun0ego@gmail.com>
2020-05-16 22:36:57 +0900

    2020/05/16 Spriteフォルダー分け 画像追加

---
+++ Assets/WorkSpace/Yui/Sprite/parrot/Parrot_Flower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parrot_Flower : MonoBehaviour
+{
+    SpriteRenderer spRend;
+    [SerializeField]
+    Sprite[] Flowers = new Sprite[10];
+    int flowerindex = 0;
+
+    [SerializeField]
+    int Timelimit = 5;
+    int time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spRend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (++time > Timelimit)
+        {
+            time = 0;
+            flowerindex = (flowerindex + 1) % 10;
+            spRend.sprite = Flowers[flowerindex];
+        }
+    }
+}
